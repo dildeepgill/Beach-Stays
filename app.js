a8b3f625c02f5c8aa5f9b68f4ab4a4034109309f
Dildeep <dildeepgill@hotmail.com>
2022-09-18 02:01:46 -0400

    finshing touches on nav toggle

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const hamburger = document.querySelector(".fa-bars")
+const hamburger = document.querySelector(".mobile-nav")
 const xMark = document.querySelector(".toggle")
 
 
-
-
 function openNav() {
-    hamburger.style.display = 'none'
-    xMark.style.display = 'block'
+    hamburger.style.visibility = "hidden"
+    hamburger.style.opacity = "0"
+    xMark.style.visibility = "visible"
+    xMark.style.opacity = "1"
+
 }
 
 function closeNav() {
-    xMark.style.display = 'none'
-    hamburger.style.display = 'block'
+    xMark.style.visibility = "hidden"
+    xMark.style.opacity = "0"
+    hamburger.style.visibility = "visible"
+    hamburger.style.opacity = "1"
 }
-
-
-
-
 
 
 hamburger.addEventListener('click', openNav)
