7ca5997b2150432ee777e67266b911a69e65f5fe
Dildeep <dildeepgill@hotmail.com>
2022-09-22 01:55:47 -0400

    added style to carosel

--- carousel.js
+++ carousel.js
@@ -1,20 +1,20 @@
 const forward = document.querySelector(".forward")
 const backward = document.querySelector(".backward")
 const img1 = document.getElementsByClassName("img-1")
-
+console.log(img1)
+console.log(img1[0])
 let imgIndex = 0
 
 const picForward = () => {
     if (imgIndex === 2) {
         imgIndex = 0
-        img1[imgIndex].style.display = "block"
-        img1[imgIndex + 2].style.display = "none"
-
+        img1[imgIndex].style.visibility = "visible"
+        img1[imgIndex + 2].style.visibility = "hidden"
 
     } else {
         imgIndex++
-        img1[imgIndex].style.display = "block"
-        img1[imgIndex - 1].style.display = "none"
+        img1[imgIndex].style.visibility = "visible"
+        img1[imgIndex - 1].style.visibility = "hidden"
     }
     console.log(imgIndex)
 
@@ -23,13 +23,13 @@ const picForward = () => {
 const picBackward = () => {
     if (imgIndex === 0) {
         imgIndex = 2
-        img1[imgIndex].style.display = "block"
-        img1[imgIndex - 2].style.display = "none"
+        img1[imgIndex].style.visibility = "visible"
+        img1[imgIndex - 2].style.visibility = "hidden"
 
     } else {
         imgIndex--
-        img1[imgIndex].style.display = "block"
-        img1[imgIndex + 1].style.display = "none"
+        img1[imgIndex].style.visibility = "visible"
+        img1[imgIndex + 1].style.visibility = "hidden"
     }
     console.log(imgIndex)
 }
